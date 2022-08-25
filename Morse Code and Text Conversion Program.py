7661ec869a12d249086cb08c6cb0766d82c10154
Muhammad Jawwad <98478303+Muhammad-Jawwad@users.noreply.github.com>
2022-08-25 17:20:54 +0500

    Add files via upload

---
+++ Morse Code and Text Conversion Program.py
@@ -0,0 +1,26 @@
+'''Writing a program that converts english text to morse code and vise versa.'''
+print("press 1 to convert english text to morse code \n press 2 to convert morse code to english text")
+op=int(input("press 1 or 2:"))
+if op==1:
+    text=input("Enter the English text which you want to convert into morse code:")
+    string=text.upper()
+    morse_code= { 'A':'.-', 'B':'-...','C':'-.-.', 'D':'-..', 'E':'.','F':'..-.', 'G':'--.', 'H':'	','I':'..'
+,'J':'.---', 'K':'-.-','L':'.-..', 'M':'--', 'N':'-.','O':'---', 'P':'.--.', 'Q':'	','R':'.-.', 'S':'	',
+'T':'-','U':'..-', 'V':'...-', 'W':'.--','X':'-..-', 'Y':'-.--', 'Z':'--..','1':'.----', '2':'..---',
+'3':'...--','4':'....-', '5':'.....', '6':'-....','7':'--...', '8':'---..', '9':'----.','0':'-----',
+', ':'--..--', '.':'.-.-.-','?':'..--..', '/':'-..-.', '-':'-....-', '(':'-.--.', ')':'-.--.-'}
+    print("The value of entered text in morse code is:")
+    for i in range(len(string)):
+        print(morse_code.get(string[i]),end=" \n ")
+elif op==2:
+    alpha_code={'.-':'A','-...':'B','-.-.':'C','-..':'D','.':'E','..-.':'F','--.':'G','....':'H','..':'I',
+'.	':'J','-.-.':'K','.-..':'L','--':'M','-.':'N','---':'O','.--.':'P','--.-':'Q', '.-.':'R','...':'S',
+'-':'T','..-':'U','...-':'V','.--':'W','-..-':'X','-.--':'Y','--..':'Z','.----':'1','..---':'2','...--':'3',
+'....-':'4','.....':'5', '-....':'6','--...':'7', '---..':'8','----.':'9','-----':'0','--..--':',',
+'.-.-.-':'.','..--..':'?','-..-.':'/','-....-':'-', '-.--.':'(','-.--.-':')'}
+    m=input("enter the morse code which you want to convert into text:")
+    code=m.split() #split() is used to separate each morse code value print("The value of Entered morse code in text is:")
+    for val in range(len(code)):
+        print(alpha_code.get(code[val]),end="")
+else:
+    print("press the correct option.")
